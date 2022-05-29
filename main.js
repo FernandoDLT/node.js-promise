7085e55d64a25a946b147d3d934c32c33e1906eb
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-05-29 08:06:04 -0500

    node.js promise

---
+++ main.js
@@ -0,0 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+    const error = true
+    if(!error) {
+        resolve('Promise has been fulfilled')
+    }else {
+        reject('Error: Operation has failed')
+    }
+})
+console.log(promise)
+promise
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
