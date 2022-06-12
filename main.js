f8800787311185fdfff5d12ef74a8d8a166fe027
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-06-11 22:06:04 -0500

    added async function using api

--- main.js
+++ main.js
@@ -11,24 +11,35 @@
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
 
-function houseOne() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 1')
-        }, 1000)
-    })
-}
-function houseOne() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 2')
-        }, 5000)
-    })
-}
-function houseOne() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 3')
-        }, 2000)
-    })
-}
+    function houseOne() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('Paper delivered to house 1')
+            }, 1000)
+        })
+    }
+    function houseTwo() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('Paper delivered to house 2')
+            }, 5000)
+        })
+    }
+    function houseThree() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('Paper delivered to house 3')
+            }, 2000)
+        })
+    }
+
+    async function getPaid() {
+        const houseOneWait = await houseOne()
+        const houseTwoWait = await houseTwo()
+        const houseThreeWait = await houseThree()
+        console.log(houseOneWait)
+        console.log(houseTwoWait)
+        console.log(houseThreeWait)
+    }
+
+    getPaid()
