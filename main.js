705dbafff20d6826745e6b54be4f25bd0048f085
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-07-07 19:17:52 -0500

    node.js promise

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-const promise = new Promise((resolve, reject) => {
-    const error = true
-    if(!error) {
-        resolve('Promise has been fulfilled')
-    }else {
-        reject('Error: Operation has failed')
-    }
-})
-console.log(promise)
-promise
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+// const promise = new Promise((resolve, reject) => {
+//     const error = true
+//     if(!error) {
+//         resolve('Promise has been fulfilled')
+//     }else {
+//         reject('Error: Operation has failed')
+//     }
+// })
+// console.log(promise)
+// promise
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+function houseOne() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 1')
+        }, 1000)
+    })
+}
+function houseOne() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 2')
+        }, 5000)
+    })
+}
+function houseOne() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 3')
+        }, 2000)
+    })
+}
